3857bd49c48649f03c5c98179d1e054aa082179c
oschuepper <osbusiness@web.de>
2023-12-19 13:49:50 +0100

    Feddisch

--- main.py
+++ main.py
@@ -7,36 +7,60 @@ class Rot13:
     def __init__(self):
         self.cipherString = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_nSkgmDJE}"
         self.cipherStringDecrypt = ""
-        self.alphaString = "abcdefghijklmnopqrstuvwxyz"
-        self.alphaList = []
-        self.alphaDict = {}
+        self.alphaStringLower = "abcdefghijklmnopqrstuvwxyz"
+        self.alphaStringUpper = self.alphaStringLower.upper()
+        self.alphaDictLower = {}
+        self.alphaDictUpper = {}
         
-        self.__create_alphaDict(self.alphaString)
+        self.__create_alphaDict(self.alphaStringLower, self.alphaStringUpper)
         self.__get_cipherChar(self.cipherString)
 
 
-    def __create_alphaDict(self, alphaString):
+    def __create_alphaDict(self, alphaStringLower, alphaStringUpper):
 
-        for index, char in enumerate(alphaString, start=1):
-            self.alphaDict.update({char : index})
+        for index, char in enumerate(alphaStringLower, start=1):
+            self.alphaDictLower.update({char : index})
+
+        for index, char in enumerate(alphaStringUpper, start=1):
+            self.alphaDictUpper.update({char : index})
         
 
     def __get_cipherChar(self, cipherString):
 
         for cipherChar in cipherString:
-            searchResult = self.alphaDict.get(cipherChar)
-            print(searchResult)
+            
+            upper = 0
+            lower = 0
+
+            if cipherChar.isupper():
+                searchResult = self.alphaDictUpper.get(cipherChar)
+                upper = 1
+                print("UPPER")
+            elif cipherChar.islower():
+                searchResult = self.alphaDictLower.get(cipherChar)
+                lower = 1
+                print("LOWER")
+            else:
+                searchResult = self.alphaDictLower.get(cipherChar)
+            
+            #print(searchResult)
+
             if searchResult == None:
-                self.cipherStringDecrypt.join(cipherChar)
+                self.cipherStringDecrypt += cipherChar
             else:
                 rot13Index = searchResult + 13
                 if rot13Index > 26:
                     rot13Index = rot13Index - 26
-                for key, value in self.alphaDict.items():
-                    if value == rot13Index:
-                        #print(key)
-                        self.cipherStringDecrypt.join(key)
-        print(self.cipherStringDecrypt)
+                if lower == 1:
+                    for key, value in self.alphaDictLower.items():
+                        if value == rot13Index:
+                            self.cipherStringDecrypt += key
+                            print(self.cipherStringDecrypt)
+                elif upper == 1:
+                    for key, value in self.alphaDictUpper.items():
+                        if value == rot13Index:
+                            self.cipherStringDecrypt += key
+                            print(self.cipherStringDecrypt)                    
                         
 
 
